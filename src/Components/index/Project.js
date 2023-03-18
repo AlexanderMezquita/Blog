98f38851d5620aaf6e355946cc55e8f40f9b9b0e
Alex <alexmezsan@gmail.com>
2023-03-17 21:06:14 -0400

    welcome page updated

--- src/Components/index/Project.js
+++ src/Components/index/Project.js
@@ -1,36 +1,44 @@
 import React from "react";
+import { ArrowBackIos } from "@mui/icons-material";
 
 export default function Project() {
   const Card = () => {
     return (
-      <div className="bg-white max-w-lg rounded-xl  container col-span-12 md:col-span-6 m-auto ">
-        <img
-          alt=""
-          src="https://media.planview.com/clarizen/wp-content/upload/2018/05/project-planning.jpg"
-          className=" h-fit"
-        />
+      <div className=" text-white max-w-lg rounded-xl col-span-12 md:col-span-6 m-auto ">
+        <div className=" rounded-xl shadow-lg shadow-[#191f29] relative  overflow-hidden">
+          <img
+            alt=""
+            src="https://media.planview.com/clarizen/wp-content/upload/2018/05/project-planning.jpg"
+            className=" hover:scale-105 hover:blur-sm rounded-xl cursor-pointer shadow-lg shadow-white   transition-all duration-300 "
+          />
+          <div className=" m-auto inset-0 opacity-0 absolute hover:opacity-100 w-full h-full transition-all duration-300">
+            <ArrowBackIos className="" />
+          </div>
+        </div>
         <div className="p-3 flex flex-col space-y-2">
-          <h2 className=" font-bold">React Space</h2>
-          <span className="">
-            Handy tool belt to create amazing AR components in a react app, with
-            reduc integration via middleware
-          </span>
-          <span className="text-xs underline cursor-pointer underline-offset-4 row-auto">
-            See more...
-          </span>
+          <h2 className="">BillCrest</h2>
+          <div className="flex flex-wrap gap-2 pt-2">
+            <Technologies title={"React"} />
+            <Technologies title={"Tailwind"} />
+            <Technologies title={"Material UI"} />
+          </div>
         </div>
       </div>
     );
   };
+
+  const Technologies = (props) => {
+    return (
+      <div className="p-2 text-white text-xs rounded-lg secondary-bg-color">
+        {props.title}
+      </div>
+    );
+  };
   return (
-    <section
-      className="w-screen bg-neutral-800 p-10 grid grid-cols-12 gap-5 row-auto "
-      id="portfolio"
-    >
-      <Card />
-      <Card />
-      <Card />
-      <Card />
+    <section className="  p-10  " id="portfolio">
+      <div className="max-w-5xl ml-auto mr-auto relative">
+        <Card />
+      </div>
     </section>
   );
 }
--- src/Components/index/Welcome.js
+++ src/Components/index/Welcome.js
@@ -1,15 +1,16 @@
 import React from "react";
-import { Button, Avatar } from "@mui/material";
-import { KeyboardArrowDown } from "@mui/icons-material";
-import js from "../assets/js-logo.svg";
-import next from "../assets/next.svg";
-import tailwind from "../assets/tailwind.svg";
-import react from "../assets/react.svg";
-import html from "../assets/html5.svg";
-import css from "../assets/css.svg";
-import redux from "../assets/redux.svg";
-import git from "../assets/git.svg";
-import me from "../assets/me.png";
+import js from "../assets/icons/js-logo.svg";
+import next from "../assets/icons/next.svg";
+import tailwind from "../assets/icons/tailwind.svg";
+import react from "../assets/icons/react.svg";
+import html from "../assets/icons/html5.svg";
+import css from "../assets/icons/css.svg";
+import redux from "../assets/icons/redux.svg";
+import git from "../assets/icons/git.svg";
+import me from "../assets/icons/me.png";
+import jira from "../assets/icons/jira.svg";
+import materialui from "../assets/icons/materialui.svg";
+
 import { GitHub, LinkedIn, FileDownload } from "@mui/icons-material";
 
 export default function Welcome() {
@@ -22,6 +23,8 @@ export default function Welcome() {
     { name: "css", src: css },
     { name: "redux", src: redux },
     { name: "git", src: git },
+    { name: "jira", src: jira },
+    { name: "materialui", src: materialui },
   ];
 
   const socialsIcons = [
@@ -33,7 +36,7 @@ export default function Welcome() {
     {
       name: "linkedin",
       icon: LinkedIn,
-      // href: "https://github.com/AlexanderMezquita",
+      href: "https://www.linkedin.com/in/alexander-m%C3%A9zquita-a9294918a/",
     },
     {
       name: "linkedin",
@@ -49,14 +52,24 @@ export default function Welcome() {
     </li>
   );
 
+  const SocialIcons = (props) => (
+    <a
+      href={props.href}
+      className="transition-all hover:scale-110 duration-300 dura cursor-pointer border-2 p-2  rounded-xl  hover:bg-white hover:text-black  border-white text-white"
+    >
+      {props.description}
+      <props.icon />
+    </a>
+  );
+
   return (
     <section
-      className=" w-screen min-h-screen  bg-black text-white p-10 pt-52"
+      className=" w-screen min-h-screen  text-white p-10 pt-52"
       id="home"
     >
       <div className="max-w-5xl ml-auto mr-auto relative">
-        <div className="  flex flex-col items-center z-10 bg-[#0D1117]  p-10  rounded-xl  ">
-          <div className="absolute -top-20 h-28 w-28  border-4 border-black  bg-[#0D1117] flex justify-center items-center rounded-full">
+        <div className="  flex flex-col items-center z-10 secondary-bg-color  p-10  rounded-xl  ">
+          <div className="absolute -top-20 h-28 w-28  border-4 border-black  secondary-bg-color flex justify-center items-center rounded-full">
             <img alt="Alex" className=" h-28 w-28 " src={me} />
           </div>
           <div className="grid grid-cols-2 gap-4">
@@ -71,25 +84,24 @@ export default function Welcome() {
               </p>
               <h3 className="mt-5 mb-2 text-center lg:text-left">Contact</h3>
               <a href="mailto:alexmezsan@gmail.com">
-                <p className=" underline hover:font-bold underline-offset-4 text-center lg:text-left mb-5">
+                <p className=" transition-all duration-100  underline hover:font-bold underline-offset-4 text-center lg:text-left mb-5">
                   alexmezsan@gmail.com
                 </p>
               </a>
-              <div className="flex flex-wrap justify-center md:justify-start w-full gap-5">
+              <div className="flex flex-wrap justify-center lg:justify-start gap-5">
                 {socialsIcons.map((item, index) => (
-                  <a
+                  <SocialIcons
                     href={item.href}
-                    className=" cursor-pointer border-2 p-2  text-center  rounded-xl  hover:bg-white hover:text-black  border-white text-white"
-                  >
-                    {item.description}
-                    <item.icon key={index} />
-                  </a>
+                    key={index}
+                    icon={item.icon}
+                    description={item.description}
+                  />
                 ))}
               </div>
             </div>
             <div className="col-span-2 md:col-span-1">
               <h2 className="mb-5 text-center lg:text-left">My skills cloud</h2>
-              <ul className="flex flex-wrap gap-2 ">
+              <ul className="flex flex-wrap gap-2 justify-center lg:justify-start ">
                 {skillsIcons.map((item, index) => (
                   <SkillsCloud key={index} src={item.src} />
                 ))}
