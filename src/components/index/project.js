5a9ae7321e38a29ee2ea9fc3d8b3b5e33d49084a
Alex <alexmezsan@gmail.com>
2023-03-24 21:42:04 -0400

    projects img fixed

--- src/components/index/project.js
+++ src/components/index/project.js
@@ -18,7 +18,7 @@ export default function Project() {
 
   const Card = (props) => {
     return (
-      <div className=" text-white  rounded-xl w-full lg:w-4/12 md:w-5/6 mx-auto lg:mx-0 lg:px-3 p-5 py-5 ">
+      <div className=" text-white  rounded-xl w-full lg:w-1/2 md:w-5/6 mx-auto lg:mx-0 lg:px-3 p-5 py-5 ">
         <div
           className=" rounded-xl group shadow-lg  dark:shadow-[#191f29] shadow-[#EDEFF2] relative cursor-pointer  overflow-hidden"
           onClick={() => props.openProject(true)}
@@ -26,7 +26,7 @@ export default function Project() {
           <img
             alt=""
             src="https://media.planview.com/clarizen/wp-content/upload/2018/05/project-planning.jpg"
-            className=" w-full group-hover:scale-105 group-hover:blur-md rounded-xl shadow-lg shadow-white transition-all duration-500  "
+            className=" w-full  group-hover:scale-105 group-hover:blur-md rounded-xl shadow-lg shadow-white transition-all duration-500  "
           />
           <div className=" inset-0 flex justify-center items-center opacity-0  absolute group-hover:opacity-100 w-full h-full transition-all duration-500">
             <div className=" rounded-full border-2  hover:scale-110 transition-all duration-300 bg-white/20 bg-opacity-100 border-white p-5 ">
@@ -69,9 +69,11 @@ export default function Project() {
           title={"Billcrest"}
           techs={["React", "Tailwind", "Material UI"]}
         />
-        {/* <Card />
-        <Card />
-        <Card /> */}
+        <Card
+          openProject={setBillcrestOpen}
+          title={"Billcrest"}
+          techs={["React", "Tailwind", "Material UI"]}
+        />
       </div>
     </section>
   );
