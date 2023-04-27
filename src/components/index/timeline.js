c134ca00cb8a09cd32a1d1e6dce474dc5b956739
Alex <alexmezsan@gmail.com>
2023-04-27 17:52:25 -0400

    dictionary updated

--- src/components/index/timeline.js
+++ src/components/index/timeline.js
@@ -11,15 +11,18 @@ import mysql from "../assets/icons/mysql.svg";
 import billcrest from "../assets/icons/billcrest.png";
 import movie from "../assets/icons/movie.svg";
 import soon from "../assets/icons/soon.svg";
+import coursera from "../assets/icons/coursera.png";
+import { useTranslation } from "react-i18next";
 
 export default function Timeline() {
+  const { t } = useTranslation();
   const elements = [
     {
       img: college,
       imgClass: "rounded-full border-2 border-[rgb(51 65 85)]",
       h3: "Web App Development - MCIT College",
-      h4: "HTML - CSS - Angular - MySQL - PHP",
-      p: "Learn technical skills and knowledge necessary to build high-quality, dynamic websites and web applications.",
+      h5: "HTML - CSS - Angular - MySQL - PHP",
+      p: t("timeline.college"),
       date: "2018 - 2019",
       background: "rgb(33, 150, 243)",
       iconBackground: "#fff",
@@ -28,8 +31,8 @@ export default function Timeline() {
       img: keywords,
       imgClass: "rounded-full ",
       h3: "FQA Tester - Keywords Studios",
-      h4: "Jira - Team work - Testing",
-      p: "Learn how to analyze and report software defects accurately using tools like Jira",
+      h5: "Jira - Team work - Testing",
+      p: t("timeline.keywords"),
       date: "2020 - 2022",
       background: "rgb(33, 150, 243)",
       iconBackground: "#fff",
@@ -38,9 +41,19 @@ export default function Timeline() {
       img: billcrest,
       imgClass: " p-2  ",
       h3: "Billcrest - Personal Project",
-      h4: "React - Material UI - Tailwind",
-      p: "Learn to build a POS system and managing invoice data using Redux",
+      h5: "React - Material UI - Tailwind",
+      p: t("timeline.billcrest"),
       date: "2022 - present",
+      background: "rgb(33, 150, 243)",
+      iconBackground: "#fff",
+    },
+    {
+      img: coursera,
+      imgClass: " p-2  ",
+      h3: "Meta Front-End Developer - Coursera",
+      h5: "Version Control - UI/UX Design - React",
+      p: t("timeline.coursera"),
+      date: "2022 - 2023",
       background: "rgb(33, 150, 243)",
       iconBackground: "#fff",
     },
@@ -48,8 +61,8 @@ export default function Timeline() {
       img: movie,
       imgClass: "mt-1 p-3",
       h3: "Movie Search App - Personal Project",
-      h4: "Vite + React - CSS ",
-      p: "Learn to paginate data and use useRef to optimize the search functionality",
+      h5: "Vite + React - CSS ",
+      p: t("timeline.moviesearch"),
       date: "2023",
       background: "rgb(33, 150, 243)",
       iconBackground: "#626a73",
@@ -58,9 +71,9 @@ export default function Timeline() {
       img: mysql,
       imgClass: "border-2 border-[rgb(51 65 85)] rounded-full",
       h3: "MySQL",
-      h4: "Database creation - Connection to the API",
-      p: "Currently learning about how to create a database and make connection to a restful API",
-      date: "Currently Learning",
+      h5: "Database creation - Connection to the API",
+      p: t("timeline.mysql"),
+      date: t("timeline.learning"),
       background: "rgb(33, 150, 243)",
       iconBackground: "#fff",
     },
@@ -68,20 +81,20 @@ export default function Timeline() {
       img: nodejs,
       imgClass: "p-2",
       h3: "Node.js",
-      h4: "Express - Routing",
-      p: "Currently learning about how to create a simple restful API using Node and Express with the best practices",
-      date: "Currently learning",
+      h5: "Express - Routing",
+      p: t("timeline.node"),
+      date: t("timeline.learning"),
       background: "rgb(33, 150, 243)",
       iconBackground: "#78B743",
     },
   ];
 
   return (
-    <section id="timeline" className=" py-0 md:py-7">
+    <section id="timeline" className=" pt-0 md:pt-7">
       <Title
-        title="Learning path"
-        description="Timeline illustrating my progress and growth in learning throughout the years."
-        backTitle="Professional Development"
+        title={t("mytimeline")}
+        description={t("timelinedes")}
+        backTitle={t("timelinebackt")}
       />
       <br></br>
       <VerticalTimeline lineColor="rgb(51 65 85)">
@@ -89,9 +102,11 @@ export default function Timeline() {
           return (
             <VerticalTimelineElement
               className="vertical-timeline-element--work test"
-              iconClassName="test"
               date={item.date}
               key={item.h3}
+              contentArrowStyle={{
+                borderRight: "8px solid  rgb(51 65 85)",
+              }}
               iconStyle={{
                 background: `${item.iconBackground}`,
                 color: "#fff",
@@ -99,7 +114,7 @@ export default function Timeline() {
               icon={<img src={item.img} className={item.imgClass} alt="" />}
             >
               <h3>{item.h3}</h3>
-              <h5>{item.h4}</h5>
+              <h5 className="text-slate-500  dark:text-slate-400">{item.h5}</h5>
               <p>{item.p}</p>
             </VerticalTimelineElement>
           );
--- src/components/index/title.js
+++ src/components/index/title.js
@@ -4,13 +4,13 @@ export default function Title(props) {
   return (
     <div className="flex flex-wrap mt-10 mb-5 md:p-3 lg:p-0 relative">
       <div className="absolute top-0 lg:-top-5 md:left-5 left-0 lg:left-5 h-32 ">
-        <h1 className=" text-5xl sm:text-6xl md:text-7xl  font-bold dark:text-slate-500/30 text-slate-700/20">
+        <h1 className=" text-3xl sm:text-6xl md:text-7xl  font-bold dark:text-slate-500/30 text-slate-700/20">
           {props.backTitle}
         </h1>
       </div>
       <div className="w-full lg:w-1/2  p-5 lg:border-l-4 border-slate-700 relative md:pl-9 pl-0 ">
-        <h1 className=" text-4xl lg:text-5xl font-bold ">{props.title}</h1>
-        <p className=" text-2xl dark:text-slate-500 text-slate-500">
+        <h1 className=" text-2xl md:text-5xl font-bold ">{props.title}</h1>
+        <p className=" text-xl dark:text-slate-500 text-slate-500">
           {props.description}
         </p>
       </div>
